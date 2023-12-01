ae1788f8e444bf4dfd71522271cfea62dbb32842
ViliusSenkus <vilius.s@outlook.com>
2023-12-01 23:34:09 +0200

    Add project files.

---
+++ 2023_11_30_ClassTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using _2023_11_30_ClassTask;
+using System;
+
+ProgressBar progressBar = new ProgressBar();
+
+async Task AddOne()
+{    
+    for (int i = 0; i < 100; i++)
+    {
+        await Task.Delay(1000);
+        progressBar.progress++;
+
+    }
+    
+}
+
+async Task ShowProgress()
+{
+    Task.Run(() => AddOne());
+    Console.WriteLine(progressBar.progress.ToString());
+    await Task.Delay(3000);
+}
+
+//async Task Start()
+//{
+
+
+
+//    ShowProgress();
+//    await Task.Run(() => AddOne());
+
+
+//}
+//await Start();
+
+ShowProgress();   
---
+++ 2023_11_30_ClassTask/ProgressBar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_11_30_ClassTask
+{
+    internal class ProgressBar
+    {
+        public int progress { get; set; } = 0;
+
+    }
+}
